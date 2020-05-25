2fa3e381a372e1f521032b9c61f322987b29a28a
Charlie Smart <charlie.r.smart@gmail.com>
2020-05-25 15:31:21 -0400

    add docs

---
+++ docs/build/precache-manifest.788574fcd47b3321ad33d0dbfba06fc6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f86642d501835add61274e791bdd7d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbff56a82a9c6661f91a",
+    "url": "/static/css/main.0cf1ba6a.chunk.css"
+  },
+  {
+    "revision": "7c80a48e51c552ee0e5c",
+    "url": "/static/js/2.97415ac6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.97415ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbff56a82a9c6661f91a",
+    "url": "/static/js/main.338f2a60.chunk.js"
+  },
+  {
+    "revision": "7562eeb02fed3f2e1bfe",
+    "url": "/static/js/runtime-main.c1650075.js"
+  },
+  {
+    "revision": "dee44d2c72b986df3aeed5ed4b02ada7",
+    "url": "/static/media/SMOG_LOOP_16-9.dee44d2c.mp4"
+  },
+  {
+    "revision": "31063db6e59399965318ff40c9ab997f",
+    "url": "/static/media/font.31063db6.woff"
+  },
+  {
+    "revision": "3859fdea9af3df8659ae41971231eb9e",
+    "url": "/static/media/font.3859fdea.woff2"
+  },
+  {
+    "revision": "58fb00ff3b0b672debdba951afc06dae",
+    "url": "/static/media/font.58fb00ff.woff"
+  },
+  {
+    "revision": "fd116e86abf6a79e596423354c0cf5f2",
+    "url": "/static/media/font.fd116e86.woff2"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e19072ae1ee9c8e592ce2da1987b0c1e",
+    "url": "/static/media/logo.e19072ae.svg"
+  }
+]);
